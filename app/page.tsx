90d36b8fcd1988fd386d9e02275468c1b8f03ad8
Beth <bethazz@hotmail.co.uk>
2024-06-07 09:39:46 +0100

    create getTranscriptButton component and update SelectedTranscript headings

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { GetTranscriptButton } from "./components/getTranscriptButton"
+
 const { TranscriptCard } = require("./components/TranscriptCard")
 const { SelectedTranscript } = require("./components/SelectedTranscript")
 
@@ -5,6 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-row items-center justify-between p-16">
       <div className="flex flex-col">
+        <GetTranscriptButton />
         <TranscriptCard />
       </div>
       <SelectedTranscript />
