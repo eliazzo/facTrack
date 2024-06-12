db36b057253825569c6f85d442d87714d0ac7f18
Beth <bethazz@hotmail.co.uk>
2024-06-12 09:59:22 +0100

    delete and replace package-lock.json

--- app/token/page.tsx
+++ app/token/page.tsx
@@ -10,19 +10,16 @@ const HandleToken = () => {
     console.log("handling token")
     const params = new URLSearchParams(window.location.search)
     const tokenFromParams = params.get("token")
-    console.log({ tokenFromParams })
+
     if (tokenFromParams) {
       setToken(tokenFromParams)
-      /* When setting the state using the setToken function from useState, the updated state (token) isn't immediately available in the same render cycle.
-      The following part where the cookie is set needs to take place in a different useEffect that runs after this one
-      */
     }
   }, [])
 
   useEffect(() => {
     if (token !== "") {
       Cookies.set("token", token, { expires: 1, secure: true })
-      console.log({ token })
+
       redirect("/")
     }
   }, [token])
