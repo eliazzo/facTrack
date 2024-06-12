4b310c92c5d66dc647b682b433e4613a5627ac0d
Beth <bethazz@hotmail.co.uk>
2024-06-12 16:21:36 +0100

    create signup route

---
+++ app/signup/page.tsx
@@ -0,0 +1,23 @@
+export default function SignUp() {
+  //   const [username, setUsername] = useState<string>("")
+  //   const [password, setPassword] = useState<string>("")
+
+  return (
+    <main className="flex min-h-screen flex-col items-center p-16">
+      <h1>sign up page</h1>
+      {/* <form onSubmit={() => handler(username, password)}>
+          <p>username</p>
+          <input
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          ></input>
+          <p>password</p>
+          <input
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+          <button>submit</button>
+        </form> */}
+    </main>
+  )
+}
