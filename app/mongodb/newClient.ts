4efce44696e49b033a5d042d01b44a6f879216f0
Beth <bethazz@hotmail.co.uk>
2024-06-12 16:25:12 +0100

    convert newClient to es6

--- app/mongodb/newClient.ts
+++ app/mongodb/newClient.ts
@@ -1,8 +1,13 @@
-require("dotenv").config()
-const { MongoClient } = require("mongodb")
+import "dotenv/config"
+import { MongoClient } from "mongodb"
 
-// Replace the uri string with your connection string.
 const uri = process.env.MONGODB_URI
+
+if (!uri) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  )
+}
 const client = new MongoClient(uri)
 
 module.exports = client
