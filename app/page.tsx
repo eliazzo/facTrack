3a9074ec88ce40d7f470638f98486ff1f2943877
Beth <bethazz@hotmail.co.uk>
2024-06-13 17:28:04 +0100

    refactor: remove console.logs and rename variables

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,20 @@
 "use client"
+import { useEffect, useState } from "react"
+import Cookies from "js-cookie"
+
 import { Button } from "./components/Button"
 import { handleClick } from "./mongodb/temporaryHandleClick"
 import { TranscriptCard } from "./components/TranscriptCard"
 import { SelectedTranscript } from "./components/SelectedTranscript"
-import { useEffect, useState } from "react"
-import Cookies from "js-cookie"
-// import { cookies } from "next/headers"
 
 export default function Home() {
   const [token, setToken] = useState("")
+
   useEffect(() => {
-    console.log("homepage")
-    const cookie = Cookies.get("token")
-    if (cookie) setToken(cookie)
-    console.log("homepage cookie: ", token)
+    const cookieToken = Cookies.get("token")
+    if (cookieToken) setToken(cookieToken)
   }, [token])
-  // console.log("homepage cookie: ", cookies().has("token"))
+
   return (
     <main className="flex min-h-screen flex-row items-center justify-between p-16">
       <div className="flex flex-col">
