ffc08886ba962c5eef74819b5f762ecfb2699b9b
Beth <bethazz@hotmail.co.uk>
2024-06-13 17:51:38 +0100

    feat: add logout function on button click

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useEffect, useState } from "react"
 import Cookies from "js-cookie"
+import { useRouter } from "next/navigation"
 
 import { Button } from "./components/Button"
 import { handleClick } from "./utils/temporaryHandleClick"
@@ -8,12 +9,18 @@ import { TranscriptCard } from "./components/TranscriptCard"
 import { SelectedTranscript } from "./components/SelectedTranscript"
 
 export default function Home() {
+  const router = useRouter()
   const [token, setToken] = useState("")
 
   useEffect(() => {
     const cookieToken = Cookies.get("token")
     if (cookieToken) setToken(cookieToken)
-  }, [token])
+  }, [])
+
+  const logout = () => {
+    Cookies.remove("token")
+    router.push("/login")
+  }
 
   return (
     <main className="flex min-h-screen flex-row items-center justify-between p-16">
@@ -22,7 +29,7 @@ export default function Home() {
         <TranscriptCard />
       </div>
       <SelectedTranscript />
-      <Button text={"Log out"} onClick={handleClick} />
+      <Button text={"Log out"} onClick={logout} />
     </main>
   )
 }
