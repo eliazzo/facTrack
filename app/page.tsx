52f48788097e7e0e96d7e74d624fc2c02b98f6a6
Beth <bethazz@hotmail.co.uk>
2024-06-24 14:49:28 +0100

    feat: update user flow and add instructions on how to use

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,16 @@ import { SelectedTranscript } from "./components/SelectedTranscript"
 import { getDocument } from "./utils/mongodb/getDocument"
 import { useEffect, useState } from "react"
 import type { Document } from "mongodb"
+import { insertDocument } from "./utils/mongodb/insertDocument"
 
 export default function Home() {
   const [notes, setNotes] = useState<Document | null>()
   const router = useRouter()
+
+  const processTranscipt = async () => {
+    await insertDocument()
+    console.log("check database for latest transcript")
+  }
 
   const getNotes = async () => {
     const latestDoc = await getDocument()
@@ -33,8 +39,26 @@ export default function Home() {
       className="flex min-h-screen flex-row items-center justify-between p-16"
     >
       <div className="flex flex-col">
+        <h1>How to use facTrack</h1>
+        <p>
+          1. Wait for google to email you notifying you of the transcript
+          creation
+        </p>
+        <p>
+          2. After receiving the email, click the Process notes button below{" "}
+        </p>
+        <Button text={"Process notes"} onClick={processTranscipt} />
+        <p>
+          3. Once you see an alert that tells you the notes have been processed,
+          click Get notes
+        </p>
         <Button text={"Get notes"} onClick={getNotes} />
-        <TranscriptCard />
+        <p>4. Download the text by clicking Download notes</p>
+        <Button
+          text={"Download notes"}
+          onClick={() => console.log("this will download text to user local")}
+        />
+        {/* <TranscriptCard /> */}
       </div>
       <SelectedTranscript latestDoc={notes} />
       <Button text={"Log out"} onClick={logout} />
