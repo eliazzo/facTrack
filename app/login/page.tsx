3cce31aacb4d445404909564a0aca91441c555af
Beth <bethazz@hotmail.co.uk>
2024-07-08 17:53:45 +0100

    remove console.logs

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,35 +11,21 @@ export default function Login() {
   async function login(formData: FormData) {
     "use server"
 
-    console.log("starting login function")
-
     const { username, password } = {
       username: formData.get("username"),
       password: formData.get("password"),
     }
-    console.log("checking for login data: ", { username, password })
 
     if (typeof username !== "string" || typeof password !== "string") {
       console.log("Invalid form data")
       return
     }
 
-    console.log("client before client.connect(): ", { client })
-
-    // await client.connect()
-
-    // console.log("client AFTER client.connect(): ", { client })
-
     const database = client.db("facTrack")
-    console.log({ database })
     const users = database.collection("users")
-
-    console.log({ users })
     const query = { username }
 
     const user = await users.findOne(query)
-
-    console.log({ user })
 
     if (!user) {
       console.log("user does not exist")
@@ -48,19 +34,18 @@ export default function Login() {
 
     const checkPassword = await bcrypt.compare(password, user.password)
 
-    console.log({ checkPassword })
-
     if (!checkPassword) {
       return
     }
 
     let token
+
     if (process.env.JWT_TOKEN) {
       token = jwt.sign({ userId: user._id }, process.env.JWT_TOKEN, {
         expiresIn: "1h",
       })
     }
-    console.log({ token })
+
     if (token) {
       cookies().set("token", token, {
         path: "/",
