cec6f6efc480838f00d0825376c8351b17953adc
Beth <bethazz@hotmail.co.uk>
2024-07-12 10:42:54 +0100

    remove transcriptCard component and test as is no longer used

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -9,6 +9,24 @@ vi.mock("next/navigation", () => ({
   }),
 }))
 
+// Mock the OpenAI initialization to prevent the error related to API key
+vi.mock("openai", () => ({
+  OpenAI: vi.fn().mockImplementation(() => ({
+    createCompletion: vi.fn().mockResolvedValue({}),
+  })),
+}))
+
+// Mock any API interactions or database functions if necessary
+vi.mock("../app/api/openai/processTranscript", () => ({
+  processTranscript: vi.fn().mockResolvedValue({}),
+}))
+
+vi.mock("../app/utils/mongodb/insertDocument", () => ({
+  insertDocument: vi.fn().mockResolvedValue({}),
+}))
+
+/* component rendering tests */
+
 test("Home component should have <main> element", () => {
   render(<Home />)
   const mainElement = document.getElementById("Home")
@@ -32,3 +50,5 @@ test("Home component should render SelectedTranscript component", () => {
   const selectedTranscript = screen.getAllByTestId("selected-transcript")
   expect(selectedTranscript).toBeDefined()
 })
+
+/* button functionality */
